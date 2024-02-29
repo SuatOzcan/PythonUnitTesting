da27f1fefc29433851f48ffcdb3c77449878d604
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-02-29 10:36:43 +0300

    Write test function for simple divide.

---
+++ functions.py
@@ -0,0 +1,7 @@
+from typing import Union
+
+def divide(dividend: Union[int, float], divisor : Union[int, float]):
+    return dividend / divisor
+
+# Testing files for files to be tested should start with "test_".
+# Testing file for this file should be "test_functions.py".
---
+++ test_functions.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from functions import divide
+
+class TestFunctions(TestCase):
+    # Functions should also start with "test_".
+    def test_divide_result(self):
+        dividend = 15
+        divisor =3
+        expected_result = 5.0
+       # self.assertEqual(divide(dividend,divisor),expected_result,
+        #                 msg="The expected result and the return of the function are not equal.")
+        # The line above is the real way, but because we are using floats,
+        # choosing the .assertAlmostEqual method is the viable option.
+        self.assertAlmostEqual(divide(dividend,divisor),expected_result,
+                         msg="The expected result and the return of the function are not equal.",
+                        delta = 0.0001)
+
+# Now type in the console python -m unittest test_functions.py
+
+# If we run the code below, it will raise an exception
+# if the expected value and the retun of the function are not equal.
+test_function = TestFunctions()
+print(test_function.test_divide_result())
+
